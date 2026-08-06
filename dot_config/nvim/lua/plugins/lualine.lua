e4feaaf2040377dea369812c9568a32c8de5b0a2
cs <12900332+headoop@users.noreply.github.com>
2026-08-06 14:20:42 +0200

    feat(lualine): implement filetype component customization in lualine

---
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,21 @@
+return {
+  -- lualine is default in layzvim. This file changes some settings of lualine.
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      -- Ersetze die filetype-Komponente
+      opts.sections.lualine_c = {
+        {
+          "filetype",
+          icon_only = false, -- Symbol + Text
+          colored = true,
+          separator = "",
+          padding = { left = 1, right = 0 },
+        },
+        unpack(vim.list_slice(opts.sections.lualine_c, 2)),
+      }
+
+      return opts
+    end,
+  },
+}
