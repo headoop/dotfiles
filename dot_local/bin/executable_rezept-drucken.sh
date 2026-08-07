7b55ed9bec1355904edab8bbfd9029db6af3f981
cs <12900332+headoop@users.noreply.github.com>
2026-08-07 18:48:53 +0200

    use xelatex
    - duplex printing is default

--- dot_local/bin/executable_rezept-drucken.sh
+++ dot_local/bin/executable_rezept-drucken.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Druckt eine Rezept-Datei: wandelt sie per pandoc/pdflatex in ein PDF um
-# (2cm Rand, Helvetica, 12pt) und leitet dieses an lpr weiter.
+# (2cm linker Rand, Helvetica, 11pt) und leitet dieses an lpr weiter.
 # Optionen: -f Datei, -d Duplexdruck, -c Farbdruck, -h Hilfe;
 # Standard ist einseitig/Graustufen auf Drucker GraustufenNormalDuplex.
 
-pdf_engine="pdflatex"
-margin_top="0mm"
+# pdf_engine="pdflatex"
+pdf_engine="xelatex" # handles UTF8 better and can use all system fonts (ttf/otf)
+margin_top="5mm"
 margin_left="20mm"
-margin_bottom="0mm"
-# font="libertine" # requires texlive-fontsextra
-font="helvet"
-fontsize="12pt"
+margin_right="5mm"
+margin_bottom="5mm"
+# fontfamily="libertine" # requires texlive-fontsextra
+# fontfamily="helvet"
+mainfont="Source Serif 4" # adobe-source-serif-fonts
+fontsize="10.8pt"
+linestretch="1"
+papersize="a4"
+pagestyle="empty" # no page numbers
 pandoc="/usr/bin/pandoc"
 lpr="/usr/bin/lpr"
 #FILE="$1"
@@ -80,7 +86,8 @@ eval set -- "$OPTS"
 # default options
 HELP=false
 FILE_SET=false
-DUPLEX="-o Duplex=None"
+# DUPLEX="-o Duplex=None"
+DUPLEX="-o Duplex=DuplexNoTumble" # print both sides per default
 COLOR="-o BRMonoColor=Mono"
 
 # processing arguments
@@ -90,8 +97,9 @@ while true; do
     HELP=true
     shift
     ;;
-  -d | --duplex)
-    DUPLEX="-o Duplex=DuplexNoTumble"
+  # -d | --duplex)
+  -o | --one-sided)
+    DUPLEX="-o Duplex=None"
     shift
     ;;
   -c | --color)
@@ -138,4 +146,15 @@ fi
 # print
 # "$pandoc" --pdf-engine="$pdf_engine" -V geometry:margin="$margin" -V fontfamily="$font" -V fontsize="$fontsize" -s "$FILE" -t pdf | "$lpr" -P "$PRINTER" "$DUPLEX" "$COLOR"
 # "$pandoc" --pdf-engine="$pdf_engine" -V geometry:top=0mm -V geometry:left=20mm -V geometry:bottom=0cm -V fontfamily="$font" -V fontsize="$fontsize" -s "$FILE" -t pdf | "$lpr" -P "$PRINTER" "$DUPLEX" "$COLOR"
-"$pandoc" --pdf-engine="$pdf_engine" -V geometry:top="$margin_top" -V geometry:left="$margin_left" -V geometry:bottom="$margin_bottom" -V fontfamily="$font" -V fontsize="$fontsize" -s "$FILE" -t pdf | "$lpr" -P "$PRINTER" "$DUPLEX" "$COLOR"
+# pandoc --pdf-engine="pdflatex" -V geometry:top="5mm" -V geometry:left="20mm" -V geometry:right="5mm" -V geometry:bottom="5mm" -V fontfamily="helvet" -V fontsize="11pt" -V papersize="a4" -s
+"$pandoc" --pdf-engine="$pdf_engine" \
+  -V geometry:top="$margin_top" \
+  -V geometry:left="$margin_left" \
+  -V geometry:right="$margin_right" \
+  -V geometry:bottom="$margin_bottom" \
+  -V mainfont="$mainfont" \
+  -V fontsize="$fontsize" \
+  -V linestretch="$linestretch" \
+  -V papersize="$papersize" \
+  -V pagestyle="$pagestyle" \
+  -s "$FILE" -t pdf | "$lpr" -P "$PRINTER" "$DUPLEX" "$COLOR"
